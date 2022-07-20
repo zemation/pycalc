d4ff1a0e853f654869aed24ae35a3d8748ad95b3
Robert Rodgers <robert.rodgers@acloudengineer.com>
2022-07-20 16:48:06 -0400

    Working version complete

--- calculator.py
+++ calculator.py
@@ -1,22 +1,49 @@
-
 def main():
+
     number1 = int(input("First Number: "))
-    operator = input("+\n-\n*\n/\n")
     number2 = int(input("Second Number: "))
-    print(calc(number1, operator, number2))
-
-def calc(num1, operator, num2):
-    if operator == '+':
-        return num1 + num2
-
-    elif operator == '-':
-        return num1 - num2
-
-    elif operator == '*':
-        return num1 * num2
-
-    elif operator == '/':
-        return num1 / num2
     
+    for operation in operations:
+        print(operation)
+    operator = input("Choose an operation above: ")
+    result = operations[operator](number1, number2)
+    print(f"{number1} {operator} {number2} = {result}")
+
+    keep_going = True
+    while keep_going: 
+        cont_op = input("Continue Operation?: [y/n]")
+        if cont_op.lower() == 'y' or cont_op.lower() == 'yes':
+            new_result = result
+            for operation in operations:
+                print(operation)
+            new_operator = input("Choose next operation: + - * /: ")
+            new_number = int(input("Choose next number: "))
+            new_result = operations[operator](result, new_number)
+            print(f"{result} {new_operator} {new_number} = {new_result}")
+        elif cont_op.lower() == 'n' or cont_op.lower() == 'no':
+            new_problem = input("New Problem?: [y/n]")
+            if new_problem == 'y' or new_problem == 'yes':
+                main()
+            else:
+                exit()
+def add(n1, n2):
+    return n1 + n2
+
+def subtract(n1, n2):
+    return n1 - n2
+
+def multiply(n1, n2):
+    return n1 * n2
+
+def divide(n1, n2):
+    return n1 / n2 
+
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide
+}
+
 if __name__ == "__main__":
     main()
